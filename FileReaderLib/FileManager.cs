1ea058353e14f1bc86a84dabe0d7fb2dcc4ddead
jmmarquezjimenez <jmmarquezjimenez@gmail.com>
2018-01-22 23:07:30 +0100

    A user should be able to read an Encrypted TEXT file

--- FileReaderLib/FileManager.cs
+++ FileReaderLib/FileManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,13 +28,96 @@ namespace FileReaderLib
         /// <summary>
         /// Reads text file (.txt) pointed in the the filePath parameter
         /// </summary>
-        public static void ReadFiles(string filePath)
+        public static void ReadFile(string filePath, int? encrptKey)
         {
-            if (filePath.EndsWith(".xml") || filePath.EndsWith(".txt"))
+            if (File.Exists(filePath) && (filePath.EndsWith(".xml") || filePath.EndsWith(".txt")))
             {
-                string contents = File.ReadAllText(filePath);
-                //Console.Write("File Content: " + contents + "\n");
+                // Not encrypted
+                if (encrptKey == null)
+                {
+                    string contents = File.ReadAllText(filePath);
+                    //Console.Write("File Content: " + contents + "\n");
+                }
+                else
+                {
+                    // Decrypt data and read
+                    string decFilePath = DecryptTextFile(filePath, (int) encrptKey);
+
+                    string contents = File.ReadAllText(decFilePath);
+                    //Console.Write("File Content: " + contents + "\n");
+                }
+
+
             }
+        }
+
+        /// <summary>
+        /// Method to create an encrypted file from the one located in filepath
+        /// </summary>
+        /// <param name="filePath">file path of the text file (.txt)</param>
+        /// <param name="key">factor that changes the encryption</param>
+        public static void EncryptTextFile(string filePath, int key)
+        {
+            if (File.Exists(filePath) && filePath.EndsWith(".txt"))
+            {
+                // Get the file name
+                string fileName = Path.GetFileName(filePath);
+
+                // Create the result File
+                var resultFile = new FileStream(AppContext.BaseDirectory + "(E)" + fileName, FileMode.OpenOrCreate, FileAccess.Write);
+                using (StreamWriter streamWriter = new StreamWriter(resultFile))
+                {
+
+                    // Read the file and encrypt it -- according to the factor
+                    var stream = File.OpenRead(filePath);
+                    using (StreamReader sr = new StreamReader(stream))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            // Write the crypted data to the new stream
+                            streamWriter.WriteLine(Convert.ToBase64String(ProtectedData.Protect(Encoding.Unicode.GetBytes(sr.ReadLine()), new byte[key], DataProtectionScope.LocalMachine)));
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method to create a decrypted file from the one located in filePath
+        /// </summary>
+        /// <param name="filePath">file path of the encrypted text data</param>
+        /// <param name="key">factor used for the encryptation</param>
+        /// <returns>the path of the new decrypted data</returns>
+        private static string DecryptTextFile(string filePath, int key)
+        {
+            if (File.Exists(filePath) && filePath.EndsWith(".txt"))
+            {
+                // Get the file name
+                string fileName = Path.GetFileName(filePath);
+                string newFileName = AppContext.BaseDirectory + fileName.Substring(3);
+
+                // Create the result File
+                var resultFile = new FileStream(newFileName, FileMode.OpenOrCreate, FileAccess.Write);
+                using (StreamWriter streamWriter = new StreamWriter(resultFile))
+                {
+
+                    // Read the file and dencrypt it -- according to the factor
+                    var stream = File.OpenRead(filePath);
+                    using (StreamReader sr = new StreamReader(stream))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            // Write the decrypted data to the new stream
+                            streamWriter.WriteLine(Encoding.Unicode.GetString(ProtectedData.Unprotect(Convert.FromBase64String(sr.ReadLine()), new byte[key], DataProtectionScope.LocalMachine)));
+                        }
+                    }
+                }
+
+                return newFileName;
+            }
+
+            return string.Empty;
+        
         }
     }
 }
